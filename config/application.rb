b8f43085637bfc6c377512160a5825a271774b05
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2020-04-11 19:48:16 -0300

    Config rspec

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module HnStories
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
